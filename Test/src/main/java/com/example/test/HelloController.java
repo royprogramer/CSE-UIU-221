8a1561a7b091f983f3e42413dac611bfbaa3ad18
arunimamithila <arunimamithila@gmail.com>
2023-03-03 12:55:16 +0600

    comment

---
+++ Test/src/main/java/com/example/test/HelloController.java
@@ -0,0 +1,14 @@
+package main.java.com.example.test;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
